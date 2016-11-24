fa7f6331be335706e110f2e2d1cd6deea8d9537e
Jakub Dutkiewicz <kubadt@gmail.com>
2016-11-24 17:21:04 +0100

    hello world

--- database_creationg.py
+++ database_creationg.py
@@ -7,9 +7,7 @@ import xml.etree.ElementTree as ET
 defs = defaults()
 working_dir = defs.root + defs.xmlexamples
 for file in listdir(working_dir):
-    iFile = open(working_dir + '/' +  file)
-    text = iFile.read()
-    xmlTree = ET.parse(text).getroot()
+    xmlTree = ET.parse(working_dir + '/' + file).getroot()
     for c in xmlTree:
         if c.tag == 'METADATA':
             print c
