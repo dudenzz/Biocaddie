279d20e8cb429a4aa5c286656f3f67c4df154720
Jakub Dutkiewicz <kubadt@gmail.com>
2016-11-24 16:50:36 +0100

    hello world

--- database_creationg.py
+++ database_creationg.py
@@ -4,7 +4,7 @@ from os import listdir
 from initials import defaults
 
 
-for file in listdir(defaults.root + defaults.xmlexamples)
+for file in listdir(defaults.root + defaults.xmlexamples):
     print file
 
 
