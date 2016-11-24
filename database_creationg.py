a9813e38496e63133a7cb584c75d4895974f93ae
Jakub Dutkiewicz <kubadt@gmail.com>
2016-11-24 17:18:40 +0100

    hello world

--- database_creationg.py
+++ database_creationg.py
@@ -2,14 +2,17 @@ __author__ = 'Jakub Dutkiewicz'
 
 from os import listdir
 from initials import defaults
-
+import xml.etree.ElementTree as ET
 
 defs = defaults()
 working_dir = defs.root + defs.xmlexamples
 for file in listdir(working_dir):
     iFile = open(working_dir + '/' +  file)
     text = iFile.read()
-    print text
+    xmlTree = ET.parse(text).getroot()
+    for c in xmlTree:
+        if c.tag == 'METADATA':
+            print c
 
 
 
