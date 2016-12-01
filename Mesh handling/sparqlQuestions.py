903904c995ddbec0f22232455d2ca637a59e8a51
dudenzz <kubadt@gmail.com>
2016-12-01 12:34:22 +0100

    adding a file

---
+++ Mesh handling/sparqlQuestions.py
@@ -0,0 +1,60 @@
+from SPARQLWrapper import SPARQLWrapper, JSON
+import sys
+
+def generatePattern(word):
+	return ""
+def getConcepts(pattern):
+	sparql = SPARQLWrapper("http://boromir.cie.put.poznan.pl:3030/Mesh/sparql")
+	q = """SELECT ?v 
+		{?v  <http://www.w3.org/2000/01/rdf-schema#label> ?word 
+		#FILTER regex(?word, pattern, "i")
+		}"""
+	print q
+	sparql.setQuery(q)
+	sparql.setReturnFormat(JSON)
+	results = sparql.query().convert()
+	rets = []
+	for i in results['results']['bindings']:
+		rets.append(i['v']['value'])
+	return rets
+	
+def getLabels(concept):
+	sparql = SPARQLWrapper("http://boromir.cie.put.poznan.pl:3030/Mesh/sparql")
+        sparql.setQuery("SELECT * {<"+concept+">  <http://www.w3.org/2000/01/rdf-schema#label> ?v}")
+        sparql.setReturnFormat(JSON)
+        results = sparql.query().convert()
+        rets = []
+        for i in results['results']['bindings']:
+                rets.append(i['v']['value'])
+	sparql.setQuery("""
+		SELECT ?v
+		WHERE
+		{
+			<"""+concept+""">  ?r ?tmp .
+			?tmp <http://www.w3.org/2000/01/rdf-schema#label> ?v
+		}""")
+	
+        sparql.setReturnFormat(JSON)
+        results = sparql.query().convert()
+        for i in results['results']['bindings']:
+                rets.append(i['v']['value'])
+	
+	sparql.setQuery("""
+                SELECT ?v
+                WHERE
+                {
+                        ?tmp ?r <"""+concept+"""> .
+                        ?tmp <http://www.w3.org/2000/01/rdf-schema#label> ?v
+                }""")
+        sparql.setReturnFormat(JSON)
+        results = sparql.query().convert()
+        for i in results['results']['bindings']:
+                rets.append(i['v']['value'])
+
+
+        return rets
+
+
+
+for c in getConcepts(sys.argv[1]):
+	print getLabels(c)
