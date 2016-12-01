eefb83f0adf006d95c40def46c2cd76622d7e2ac
Jakub Dutkiewicz <kubadt@gmail.com>
2016-12-01 17:20:19 +0100

    creating mesh dictionary

--- clean_geo.py
+++ clean_geo.py
@@ -11,7 +11,7 @@ def cleanhtml(raw_html):
   cleanr = re.compile('<.*?>')
   cleantext = re.sub(cleanr, '', raw_html)
   return cleantext
-htmlparser = etree.HTMLParser
+htmlparser = etree.HTMLParser()
 for file in listdir(defs.root+defs.geohtmls):
     root = etree.parse(file,htmlparser)
     ctable = root[1][4][0][0][5][2][1][0][0][0][0][0][5][0][0][0]
